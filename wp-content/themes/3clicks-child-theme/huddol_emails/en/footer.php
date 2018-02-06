967d75907e0ac4a82206e5eca28e6dcfb28cb58f
Warren Knox <warren.knox@gmail.com>
2018-02-06 16:48:49 -0500

    Update styles and email footer

--- wp-content/themes/3clicks-child-theme/huddol_emails/en/footer.php
+++ wp-content/themes/3clicks-child-theme/huddol_emails/en/footer.php
@@ -2,7 +2,7 @@
 
 		</div>
 		
-		<img src="http://thecaregivernetwork.ca:8888/wp-content/themes/3clicks-child-theme/images/huddol-logo-header.png" style="padding-top: 30px;">
+		<img src="http://events.huddol.com/wp-content/themes/3clicks-child-theme/images/huddol-logo-header.png" style="padding-top: 30px;">
 		
 		<p>
 			This email was sent to <a href="<?php echo $email_address ?>" style="color:#25aae1;">
