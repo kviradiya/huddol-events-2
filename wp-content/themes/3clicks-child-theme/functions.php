5b4a7fe9ceaba8ab8b05692e40f9473f45cfa22e
Warren Knox <warren.knox@gmail.com>
2018-02-12 10:58:26 -0500

    Update reminder handler to not echo message to page.

--- wp-content/themes/3clicks-child-theme/functions.php
+++ wp-content/themes/3clicks-child-theme/functions.php
@@ -2120,13 +2120,10 @@ function check_events_and_send_emails()
 
     $message .= '</pre>';  
     
-    echo $message;
-    
     global $wpdb;
     $results = $wpdb->get_results("INSERT INTO wp_tcn_cron_runs () VALUES ()");
-    echo send_email('events@huddol.com', 'Huddol Events: Surveys and Reminders Summary', $message);
-    echo send_email('events@huddol.com', 'Huddol Events: Surveys and Reminders Actually Sent', $sent_message);
-    echo '<br/>';
+    send_email('events@huddol.com', 'Huddol Events: Surveys and Reminders Summary', $message);
+    send_email('events@huddol.com', 'Huddol Events: Surveys and Reminders Actually Sent', $sent_message);    
 }
 
 
