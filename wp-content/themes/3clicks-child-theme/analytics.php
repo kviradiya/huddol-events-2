25daa7a4a0b21ca2c59b51e074ff75c5c6542a47
aronblack <aronblack@bell.net>
2018-06-17 00:53:55 -0400

    fix google analytics

--- wp-content/themes/3clicks-child-theme/analytics.php
+++ wp-content/themes/3clicks-child-theme/analytics.php
@@ -1,21 +1,30 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async
+        src="https://www.googletagmanager.com/gtag/js?id=UA-114911874-1"></script>
 <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  window.dataLayer = window.dataLayer || [];
 
-  ga('create', 'UA-114911874-1', 'auto');
-  ga('send', 'pageview');
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+
+  gtag('js', new Date());
+
+  gtag('config', 'UA-114911874-1');
 </script>
 
-<script type="text/javascript">
-	jQuery(document).ready(function($) {
-		// Track AdobeConnect Clicks
-		$('a[href*="adobeconnect.com"]').on('click', function(e) {
-			if(ga) {
-				ga('send', 'event', { eventCategory: 'AdobeConnect', eventAction: 'Click', eventLabel: $(e.currentTarget).attr('href') + ' ' + document.title});
-			}
-		});
 
-	});
+<script type="text/javascript">
+  jQuery(document).ready(function ($) {
+    // Track AdobeConnect Clicks
+    $('a[href*="adobeconnect.com"]').on('click', function (e) {
+      if (gtag) {
+        gtag('event', 'Click', {
+          'event_category': 'AdobeConnect',
+          'event_label': $(e.currentTarget).attr('href') + ' ' + document.title
+        });
+      }
+    });
+
+  });
 </script>
